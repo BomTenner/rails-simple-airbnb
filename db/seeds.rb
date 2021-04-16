b3a210b1b3d10111d1898175755430233eeec8e5
Tom Benner <tombenner@web.de>
2021-04-16 13:54:02 +0100

    Functionality is working, design missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating dummy flats"
+
+4.times do
+  flat = Flat.new(
+    name: Faker::Cannabis.strain,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    price_per_night: [25, 35, 45, 55, 65, 75].sample,
+    number_of_guests: [1, 2, 3, 4, 5].sample
+  )
+  flat.save!
+end
+
+puts "Finished"
